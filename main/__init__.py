94ee4f0e7b96a5bc48416193e8734d6f571b5d11
Hevrji <116875997+Hevrji@users.noreply.github.com>
2023-09-30 20:26:14 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,10 +12,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
+API_ID = config("28414026", default=None, cast=int)
+API_HASH = config("595e101e4009da570ae71cae7060d20f", default=None)
+BOT_TOKEN = config("6452140286:AAEAOTxVxbiu2i77NnaiMJRfZDExzMrqAME", default=None)
+SESSION = config("BQGxkEoASPLfn7uj75CM_PSsSplXM-RCHVobIYZcRNEyCKgH7s4tMlzb1tDY67uDCv-uDTi5EhL3JhCWFlcFufqXwUl5UrrgWRezCJDnEp0RRdnrizgZmmrqKwY3AMYa1iRy86ZDLuYR3Oji54lTy0s0ktAQ8qkCG2xPJqXbPTuc14ROuLnPrx8Yw9UsDeWxtQf_80JRhK8kYNJKf9SkQj6Ap7jhYA68bgLaTb7N9pS1yz4NdW5A4W1A3zR4vh2p30vhqrQbic0SHuGf778fWSluyp78XU-vI8WSZSoh3rowv3HQa5IpY3rHgI8Rkf1rhsCHz6bZYTfvOjEU8eJr9z7_rre1HAAAAABX-gQfAA", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
